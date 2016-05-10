b84b33b9b701ebe3483a723213bb61d35ca05e4b
ruby -v ruby -v <ajhofferber@gmail.com>
2016-05-10 17:16:53 -0400

    adds refactored code to scrape show data

--- lib/capybara_phantomjs.rb
+++ lib/capybara_phantomjs.rb
@@ -1,24 +1,37 @@
+require 'capybara'
+require 'capybara/dsl'
 require 'capybara/poltergeist'
 
-class Scraper
+Capybara.default_driver = :poltergeist
+Capybara.run_server = false
 
-  def initialize
-    Capybara.register_driver :poltergeist do |app|
-      Capybara::Poltergeist::Driver.new(app, js_errors: false)
-    end
-
-    Capybara.default_driver = :poltergeist
-  end
-
-  def get_url
-
-    browser = Capybara.current_session
-    url = "http://www.ohmyrockness.com/shows"
-    browser.visit url
-
-   browser.all('div').each { |div| puts div.text }
-  end
-
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--load-images=false', '--disk-cache=false'])
 end
 
-scrape = Scraper.new
+module GetPage
+  class WebScraper
+    include Capybara::DSL
+
+    def get_rockness_page(url)
+      visit(url)
+      doc = Nokogiri::HTML(page.html)
+
+      rows = doc.css('.vevent')
+
+      rows.each do |row|
+        puts row.css('.dtstart').text
+        puts row.css('.bands').text
+        puts row.css('.venue').text
+
+      end
+
+    end
+  end
+end
+
+url = "http://www.ohmyrockness.com/shows"
+puts "Downloading #{url}"
+
+scraper = GetPage::WebScraper.new
+puts scraper.get_rockness_page(url)
